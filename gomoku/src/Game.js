4fab02b93c624c49e4be01f20e0ae133b99c6805
Hae-Ji Park <positive235@gmail.com>
2020-07-06 14:27:10 -0700

    put stones onto the proper places

--- gomoku/src/Game.js
+++ gomoku/src/Game.js
@@ -7,6 +7,8 @@ class Game extends React.Component {
     this.init = this.init.bind(this);
     this.addGo = this.addGo.bind(this);
     this.blackStone = true;
+    this.stoneRadius = 25;
+    this.stoneDiameter = this.stoneRadius * 2;
   }
 
   componentDidMount() {
@@ -37,24 +39,42 @@ class Game extends React.Component {
 
   addGo(event){
     // calculating exact mouse coordinates
-    this.rect.x = event.pageX - this.goBoardX - 1.2;
+    this.rect.x = event.pageX - this.goBoardX - 1;
     this.rect.y = event.pageY - this.goBoardY - 2;
-
-    // draw stones based on current mouse position
-    this.context.beginPath();
-    this.context.arc(this.rect.x, this.rect.y, 25, 0, 2 * Math.PI);
-
-    // fill the color of stone
-    if (this.blackStone == true) {
-      this.context.fillStyle = "black";
-      this.blackStone = false;
-    } else {
-      this.context.fillStyle = "white";
-      this.blackStone = true;
-    }
     
-    this.context.fill();
-    this.context.stroke();
+    // calculating coordinates to put stones onto the proper places
+    if (this.rect.x % this.stoneDiameter > this.stoneRadius) {
+      this.rect.x = (this.rect.x - this.rect.x % this.stoneDiameter) + this.stoneDiameter;
+    } else {
+      this.rect.x -= this.rect.x % this.stoneDiameter;
+    }
+
+    if (this.rect.y % this.stoneDiameter > this.stoneRadius) {
+      this.rect.y = (this.rect.y - this.rect.y % this.stoneDiameter) + this.stoneDiameter;
+    } else {
+      this.rect.y -= this.rect.y % this.stoneDiameter;
+    }
+
+    // put stones only if they are not on the edges
+    if (this.rect.x > 0 && this.rect.x < 700
+      && this.rect.y > 0 && this.rect.y < 700) {
+
+      // put stones based on thier calculated coordinates
+      this.context.beginPath();
+      this.context.arc(this.rect.x, this.rect.y, this.stoneRadius, 0, 2 * Math.PI);
+
+      // fill the color of stone:  black or white
+      if (this.blackStone === true) {
+        this.context.fillStyle = "black";
+        this.blackStone = false;
+      } else {
+        this.context.fillStyle = "white";
+        this.blackStone = true;
+      }
+
+      this.context.fill();
+      this.context.stroke();
+    } 
   }
 
   render() {
