0d077b0d6830bb86058705bbbf7eb4e8f8f84166
Hae-Ji Park <positive235@gmail.com>
2020-07-06 11:47:25 -0700

    made board and generate stones when clicking the board

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# gomoku-react
-IN PROGRESS
+# GOMOKU in React JS (IN PROGRESS)
+
+This is a traditional Gomoku game(five-in-a-row) played by 2 players. 
+
+```
+Gomoku from wiki: Gomoku, also called Five in a Row, is an abstract strategy board game. Players alternate turns placing a stone of their color on an empty section. The winner is the first player to form an unbroken chain of five stones horizontally, vertically, or diagonally. (Reference: https://en.wikipedia.org/wiki/Gomoku)
+```
+
+## Tutorial: GOMOKU in React JS
+
+### Start (reference: https://reactjs.org/tutorial/tutorial.html)
+
+1. Make sure you have a recent version of Node.js installed.
+2. Install Create React App to make a new project. In terminal, `npx create-react-app my-app`
+3. Delete all files in the `src/` folder of the new project. (Note: Don't delete the entire src folder, just the original source files inside it).
+
+4. `cd my-app`
+5. `cd src`
+6. If you are using Mac or Linux: `rm -f *` / Or, if you're on Windows: `del *`
+8. Then, switch back to the project folder: `cd ..`
+9. Add three files named `index.css`, `index.js`, `Game.js` in the `src/` folder.
+
+10. Add these lines to the top of `index.js` in the `src/` folder:
+
+```
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import './index.css';
+```
+
+11. Now if you run `npm start` in the project folder and open `http://localhost:3000` in the browser, you should see an empty gomoku field.
+
+
+(In Progress...)
---
+++ gomoku/src/Game.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.init = this.init.bind(this);
+    this.addGo = this.addGo.bind(this);
+  }
+
+  componentDidMount() {
+    this.goBoard = document.getElementById('goBoard');
+    this.context = this.goBoard.getContext('2d');
+
+    // draw multiple lines for go board
+    for (let i= 0; i < 15; i++) {
+      this.context.moveTo(0, 50*i);
+      this.context.lineTo(700, 50*i);
+      this.context.moveTo(50*i, 0);
+      this.context.lineTo(50*i, 700);
+    }
+    this.context.stroke();
+
+    // for mouse coordinate
+    this.rect = {};
+    this.goBoardX = this.goBoard.offsetLeft;
+    this.goBoardY = this.goBoard.offsetTop;
+
+    this.init();
+  }
+
+  init() {
+    //when clicking the board, a stone will be added to the board
+    this.goBoard.addEventListener('mousedown', this.addGo, false);
+  }
+
+  addGo(event){
+
+    // calculating exact mouse coordinates
+    this.rect.x = event.pageX - this.goBoardX- 1.2;
+    this.rect.y = event.pageY - this.goBoardY- 2;
+
+    // draw stones based on current mouse position
+    this.context.beginPath();
+    this.context.arc(this.rect.x, this.rect.y, 25, 0, 2*Math.PI);
+
+    // fill the color of stone
+    this.context.fillStyle="black";
+    this.context.fill();
+
+    this.context.stroke();
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <canvas id="goBoard" width="700" height="700" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
