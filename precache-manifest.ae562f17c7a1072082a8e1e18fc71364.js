615d505842e00ebe5342a6514334e12398fc48b4
Hae-Ji Park <positive235@gmail.com>
2020-07-07 14:31:49 -0700

    Updates

---
+++ precache-manifest.ae562f17c7a1072082a8e1e18fc71364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64ecb8c44f24075ac5ec7ceb06e4a99",
+    "url": "/gomoku-react/index.html"
+  },
+  {
+    "revision": "100315538549507e352f",
+    "url": "/gomoku-react/static/css/main.898bab81.chunk.css"
+  },
+  {
+    "revision": "99c49a45acbf63f10483",
+    "url": "/gomoku-react/static/js/2.c13d6821.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gomoku-react/static/js/2.c13d6821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100315538549507e352f",
+    "url": "/gomoku-react/static/js/main.a98a5b0f.chunk.js"
+  },
+  {
+    "revision": "3c6420d74fdf3a7d0350",
+    "url": "/gomoku-react/static/js/runtime-main.8ab5e4e7.js"
+  }
+]);
